75518337c1ecddd1665013d3c085ee8d3513c663
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-12-03 19:32:39 -0500

    Added encoder variables, PID variables, and drive power setters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TileRunnerHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TileRunnerHardware.java
@@ -12,6 +12,22 @@ public class TileRunnerHardware {
     public DcMotor rearRight;
 
 
+    public final double COUNTS_PER_REV_HD_20    = 560; // REV HD Hex 20:1 motor
+    public final double DRIVE_GEAR_REDUCTION    = 20.0 / 26.0; // 15 tooth on motor shaft to 15 tooth on wheel shaft
+    public final double WHEEL_DI_INCHES         = 90.0 / 25.4; // 90mm diameter wheel divided by 25.4(in/mm)
+    public final double COUNTS_PER_INCH         = (COUNTS_PER_REV_HD_20 * DRIVE_GEAR_REDUCTION) / (WHEEL_DI_INCHES * Math.PI);
+
+
+    // PID variables
+    public final double MAX_SPEED = 0.4;
+    public final double P = 0.02;
+    public final double I = 0.0;
+    public final double D = 0.1;
+    public final double TOLERANCE = 1;
+
+    public final SynchronousPID pid = new SynchronousPID(P, I, D);
+
+
     public void init(HardwareMap hardwareMap) {
         frontLeft   = hardwareMap.dcMotor.get("FL");
         frontRight  = hardwareMap.dcMotor.get("FR");
@@ -26,6 +42,16 @@ public class TileRunnerHardware {
         frontRight. setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearLeft.   setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearRight.  setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setLeftPower(double power) {
+        frontLeft.setPower(power);
+        rearLeft.setPower(power);
+    }
+
+    public void setRightPower(double power) {
+        frontRight.setPower(power);
+        rearRight.setPower(power);
     }
 
 }
